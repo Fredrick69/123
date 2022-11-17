fac4c3b3f813403614187649986ea62637087419
Fredrick69 <94642416+Fredrick69@users.noreply.github.com>
2022-11-17 11:49:19 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+ function setup()
+ {
+    video=createCapture(VIDEO);
+    video.size(580, 530);
+     canvas.center()
+    canvas=createCanvas(600, 550);
+    
+ }
